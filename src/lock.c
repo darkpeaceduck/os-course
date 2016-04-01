c15b531b693a1716ede30b636f23e9673c3d48a2
sanya1 <okay123456@yandex.ru>
2016-04-02 00:10:49 +0300

    fixed locks

--- src/lock.c
+++ src/lock.c
@@ -21,7 +21,7 @@ void lock(void * arg_lock) {
 	}
 	ticket_lock * lock = (ticket_lock *)arg_lock;
 	if(lock->global){
-		sti();
+		cli();
 	}
 	const uint16_t ticket = __sync_fetch_and_add(&lock->users, 1);
 	while(lock->ticket != ticket) {
@@ -38,7 +38,7 @@ void unlock(void * arg_lock) {
 	__sync_synchronize();
 	__sync_add_and_fetch(&lock->ticket, 1);
 	if(lock->global) {
-		cli();
+		sti();
 	}
 }
 
